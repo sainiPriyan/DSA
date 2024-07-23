c0bce50f89e1039dca3f215efd72b7f992c4d020
Priyanshu Saini <141767707+sainiPriyan@users.noreply.github.com>
2024-07-23 18:00:39 +0530

    Update Main.java

--- GeeksForGeeksProblems/prime/src/Main.java
+++ GeeksForGeeksProblems/prime/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static boolean isPrime(int num){
 
         if(num == 1 || num ==0 ) return false;
-        else if(num == 2) return true;
+        else if(num == 2 || num ==3) return true;
         else if (num%2 == 0 || num%3 == 0) return false;
 
         for (int i = 5; i*i<=num;i+=6){
