864ddcfc2b16eda4bb3dd6548cdd9201b0e8f581
Priyanshu Saini <priyanshusaini124@gmail.com>
2025-05-15 19:16:16 +0530

    Deque

---
+++ deque/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque deque = new Deque(5);
+        deque.insertFront(4);
+        deque.insertFront(5);
+        deque.insertRear(1);
+        deque.insertRear(2);
+        deque.insertRear(3);
+
+        deque.printDequeArray();
+
+    }
+}
+
+class Deque{
+    int size,front,rear;
+    int capacity;
+    int [] arr;
+    Deque(int capacity){
+        this.capacity = capacity;
+        size = 0;
+        front = rear = 0;
+        arr = new int[capacity];
+    }
+
+    public void insertFront(int x){
+        if(size<capacity){
+            arr[front] = x;
+            front = (front+1)%capacity;
+            size++;
+        }
+        else System.out.println("No space to insert");
+    }
+
+    public void removeRear(){
+        
+    }
+
+    public void insertRear(int x){
+        if(size<capacity){
+            rear = (capacity+rear-1)%capacity;
+            arr[rear] = x;
+            size++;
+        }
+        else System.out.println("No space to insert");
+    }
+
+    public void printDequeArray(){
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[rear]+" ");
+            rear=(rear+1)%capacity;
+        }
+        System.out.println();
+
+        System.out.print("Array Structure: ");
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
